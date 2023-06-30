0973bf3afdf597927d915546476c38cc1434ca5d
Apoezon <yarosvet@bk.ru>
2023-06-30 17:46:45 +0400

    Practice 1 - done!

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import Login
+from .views import Login, LogoutView, CreateAccountView
 
+app_name = 'login'
 
 urlpatterns = [
-    path('', Login.as_view()),
+    path('', Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create/', CreateAccountView.as_view(), name="create"),
 ]
--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'other',
     'login',
-    'store'
+    'store',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+   'store.backends.CustomAuthBackend',
+   'django.contrib.auth.backends.ModelBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -119,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
